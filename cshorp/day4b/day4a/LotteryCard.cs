be09f602912725852d475ff3c651baa5d25e0db6
Jared M Ashcraft <jaredmashcraft@gmail.com>
2024-01-05 12:29:15 -0500

    Finished day4a, started day4b

---
+++ cshorp/day4b/day4a/LotteryCard.cs
@@ -0,0 +1,47 @@
+﻿namespace day4b;
+
+public class LotteryCard
+{
+    public LotteryCard(string rawLine, List<LotteryCard> uniqueCards)
+    {
+        _uniqueCards = uniqueCards;
+        var splitByColon = rawLine.Split(":");
+
+        Id = splitByColon[0].Split()[1];
+
+        WinningNumbers = ParseNumberGroup(rawLine, 0);
+        
+        OurNumbers = ParseNumberGroup(rawLine, 1);
+
+        InstanceNextChildCards();
+    }
+
+
+    public string Id { get; }
+
+    public string[] OurNumbers { get; }
+
+    public string[] WinningNumbers { get; }
+
+    public static int CardCount { get; private set; } = 0;
+
+    public List<LotteryCard> ChildCards { get; } = new List<LotteryCard>();
+
+    private static List<LotteryCard> _uniqueCards = new List<LotteryCard>();
+
+    private void InstanceNextChildCards()
+    {
+        
+    }
+
+    private string[] ParseNumberGroup(string rawLine, int groupIndex)
+    {
+        var splitByColon = rawLine.Split(":");
+        
+        var numbersRaw = splitByColon[1].Split("|")[groupIndex];
+        
+        var numbersRawArray = numbersRaw.Split();
+
+        return numbersRawArray.Where(n => !string.IsNullOrWhiteSpace(n)).ToArray();
+    }
+}
